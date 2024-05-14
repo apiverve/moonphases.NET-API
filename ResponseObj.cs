f7606e02d6743b24c597817b62d29c5786a1f107
APIVerve Team <hello@apiverve.com>
2024-05-14 15:36:15 -0500

    Bug fixes and Improvements

--- ResponseObj.cs
+++ ResponseObj.cs
@@ -1,21 +1,53 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
+using Newtonsoft.Json;
 
 namespace APIVerve
 {
-    using Newtonsoft.Json;
+public class data
+{
+    [JsonProperty("phase")]
+    public string phase { get; set; }
+
+    [JsonProperty("phaseEmoji")]
+    public string phaseEmoji { get; set; }
+
+    [JsonProperty("waxing")]
+    public bool waxing { get; set; }
+
+    [JsonProperty("waning")]
+    public bool waning { get; set; }
+
+    [JsonProperty("lunarAge")]
+    public double lunarAge { get; set; }
+
+    [JsonProperty("lunarAgePercent")]
+    public double lunarAgePercent { get; set; }
+
+    [JsonProperty("lunationNumber")]
+    public int lunationNumber { get; set; }
+
+    [JsonProperty("lunarDistance")]
+    public double lunarDistance { get; set; }
+
+    [JsonProperty("nextFullMoon")]
+    public DateTime nextFullMoon { get; set; }
+
+    [JsonProperty("lastFullMoon")]
+    public DateTime lastFullMoon { get; set; }
+
+}
 
 public class ResponseObj
 {
     [JsonProperty("status")]
-    public string Status { get; set; }
+    public string status { get; set; }
 
     [JsonProperty("error")]
-    public string Error { get; set; }
+    public object error { get; set; }
 
     [JsonProperty("data")]
-    public object Data { get; set; }
+    public data data { get; set; }
 
 }
 
